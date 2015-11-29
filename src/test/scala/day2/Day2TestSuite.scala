6d57a1fe2e72c6bd18fe9d1081e760483cd6f8c8
ktr-skmt <sakamoto_911@hotmail.com>
2015-11-30 01:57:23 +0900

    update

---
+++ src/test/scala/day2/Day2TestSuite.scala
@@ -0,0 +1,268 @@
+package day2
+
+import java.io.File
+
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+
+/**
+  * @author ynupc
+  *         Created on 2015/11/25
+  */
+class Day2TestSuite extends AssertionsForJUnit {
+  @Test
+  def testLiteral(): Unit = {
+    //空リテラル
+    //Java:
+    //Object n = null;
+    //Scala:
+    val n: Object = null
+
+    //文字リテラル
+    //Java:
+    //char c = ‘A’;
+    //Scala:
+    val c: Char = 'A'
+
+    //文字列リテラル
+    //Java:
+    //String s = "AB¥¥C¥nあいう";
+    //Scala:
+    val s1: String = "AB\\C\nあいう"
+
+    //生文字リテラル
+    //Java:
+    //なし
+    //Scala:
+    val s2: String = """AB\C
+あいう"""
+  }
+
+  @Test
+  def testRawStringLiteral(): Unit = {
+    val sStringLiteral: String = "AB\\C\nあいう"
+
+    val sRawStringLiteral: String = """AB\C
+あいう"""
+
+    val waveDashStringLiteral: String = "\u301c"
+
+    val waveDashRawStringLiteral: String = """\u301c"""
+
+    assert(sStringLiteral.toString == sRawStringLiteral)
+    assert(waveDashStringLiteral == waveDashRawStringLiteral)
+  }
+
+  @Test
+  def testStripMargin(): Unit = {
+    val s: String = """AB\C
+あいう
+イロハ"""
+
+    val s1: String = """AB\C
+                       |あいう
+                       |イロハ""".stripMargin
+
+    val s2: String = """AB\C
+                       %あいう
+                       %イロハ""".stripMargin('%')
+
+    assert(s == s1)
+    assert(s == s2)
+  }
+
+  @Test
+  def testSInterpolation(): Unit = {
+    val universe = "宇宙"
+    val result = s"生命、$universe、そして${s"万物についての${"究極の"}疑問"}の答えは、${21 + 21}"
+
+    assert(result == "生命、宇宙、そして万物についての究極の疑問の答えは、42")
+  }
+
+  @Test
+  def testFInterpolation(): Unit = {
+    val result = f"サイボーグ${9}%03dVSデビルマン"
+
+    assert(result == "サイボーグ009VSデビルマン")
+  }
+
+  @Test
+  def testRawInterpolation(): Unit = {
+    val sStringLiteral:    String = "AB\\C\\nあいう"
+    val sRawInterpolation: String = raw"AB\C\nあいう"
+    val sRawLiteral:       String = """AB\C\nあいう"""
+
+    assert(sStringLiteral == sRawInterpolation)
+    assert(sRawInterpolation == sRawLiteral)
+    assert(sStringLiteral == sRawLiteral)
+
+    val waveDashStringLiteral:   String = "\u301c"
+    val waveDashRawIntepolation: String = raw"\u301c"
+    val waveDashRawLiteral:      String = """\u301c"""
+
+    assert(waveDashStringLiteral == waveDashRawIntepolation)
+    assert(waveDashRawIntepolation == waveDashRawLiteral)
+    assert(waveDashStringLiteral == waveDashRawLiteral)
+  }
+
+  @Test
+  def testDifferenceBetweenRawInterpolationAndRawStringLiteral(): Unit = {
+    //val newLineRawInterpolation: String = raw"改
+//行"
+    val newLineRawStringLiteral: String = """改
+行""".stripMargin
+
+    //val doubleQuotationRawInterpolation: String = raw"ダブルクォーテーション「"」"
+    val doubleQuotationRawStringLiteral: String = """ダブルクォーテーション「"」"""
+  }
+
+  @Test
+  def testStringContext(): Unit = {
+    assert(Day2TestStringContext.test() == "{ name: 名前, id: ID }")
+  }
+
+  @Test
+  def testEscapeSequence(): Unit = {
+    //バックスペース
+    val backSpace = '\b'
+    //改ページ
+    val formFeed = '\f'
+    //改行
+    val newLine = '\n'
+    //復帰
+    val carriageReturn = '\r'
+    //水平タブ
+    val horizontalTab = '\t'
+    //バックスラッシュ
+    val backSlash = '\\'
+    //シングルクォーテーション
+    val singleQuotation = '\''
+    //ダブルクォーテーション
+    val doubleQuotation = '\"'
+    //８進数
+    val octalNumberOfZero = '\000'
+    val octalNumberOfTwoHundredFiftyFive = '\377'
+
+    assert(octalNumberOfZero == 0)
+    assert(octalNumberOfTwoHundredFiftyFive == 255)
+  }
+
+  @Test
+  def testUnicodeSequence(): Unit = {
+    //同上記号「〃」
+    val dojo1 = '\u3003'
+
+    assert(dojo1 == '〃')
+
+    //漢字連続記号「々」
+    val kanjiRenzoku = '\u3005'
+
+    assert(kanjiRenzoku == '々')
+
+    //しめ記号「〆」
+    val shime = '\u3006'
+
+    assert(shime == '〆')
+
+    //漢数字ゼロ「〇」
+    val zero = '\u3007'
+
+    assert(zero == '〇')
+
+    //波ダッシュ「〜」
+    val waveDash = '\u301C'
+
+    assert(waveDash == '〜')
+
+    //中点「・」
+    val chuten = '\u30FB'
+
+    assert(chuten == '・')
+
+    //同上記号「仝」
+    val dojo2 = '\u4EDD'
+
+    assert(dojo2 == '仝')
+
+    //幽霊文字「妛」
+    val yurei1 = '\u599B'
+
+    assert(yurei1 == '妛')
+
+    //幽霊文字「彁」
+    val yurei2 = '\u5F41'
+
+    assert(yurei2 == '彁')
+
+    //全角チルダ「～」
+    val fullWidthTilde = '\uFF5E'
+
+    assert(fullWidthTilde == '～')
+
+    //𠮷野家の「𠮷」
+    val yoshi = "\uD842\uDFB7"
+
+    assert(yoshi == "𠮷")
+  }
+
+  @Test
+  def testOSDependentCharacter(): Unit = {
+    //改行
+    //Windows: \r\n
+    //UNIX:      \n
+    val newLine1 = System.getProperty("line.separator")
+    val newLine2 = System.lineSeparator()
+    val newLine3 = String.format("%n")
+
+    assert(newLine1 == newLine2)
+    assert(newLine2 == newLine3)
+    assert(newLine1 == newLine3)
+
+    //ディレクトリやファイルのパスの区切り
+    //Windows: \\
+    //UNIX:     /
+    val fileSeparator1 = System.getProperty("file.separator")
+    val fileSeparator2 = File.separator
+
+    assert(fileSeparator1 == fileSeparator2)
+
+    //PATHやCLASSPATHの区切り
+    //Windows: ;
+    //UNIX:    :
+    val pathSeparator1 = System.getProperty("path.separator")
+    val pathSeparator2 = File.pathSeparator
+
+    assert(pathSeparator1 == pathSeparator2)
+  }
+}
+
+object Day2TestStringContext {
+  def test(): String = {
+    val name = "名前"
+    val id = "ID"
+    val x: JSONObject = json"{ name: $name, id: $id }"
+    x.toString
+  }
+
+  class JSONObject(jsonText: String) {
+    override def toString: String = jsonText
+  }
+
+  implicit class JsonHelper(val sc: StringContext) extends AnyVal {
+    def parseJson(charSequence: CharSequence): JSONObject = {
+      new JSONObject(charSequence.toString)
+    }
+
+    def json(args: Any*): JSONObject = {
+      val strings = sc.parts.iterator
+      val expressions = args.iterator
+      val buf = new StringBuffer(strings.next)
+      while (strings.hasNext) {
+        buf.append(expressions.next).
+          append(strings.next)
+      }
+      parseJson(buf)
+    }
+  }
+}
+
---
+++ src/test/scala/day3/Day3TestSuite.scala
@@ -0,0 +1,375 @@
+package day3
+
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+
+/**
+  * @author ynupc
+  *         Created on 2015/11/25
+  */
+class Day3TestSuite extends AssertionsForJUnit {
+  @Test
+  def testSurrogatePairToCodePoint(): Unit = {
+    val high: Char = 0xD842
+    val low : Char = 0xDFB7
+    assert(Character.isHighSurrogate(high))
+    assert(Character.isLowSurrogate(low))
+    if (Character.isHighSurrogate(high) && Character.isLowSurrogate(low)) {
+      val codePoint: Int = Character.toCodePoint(high, low)
+
+      assert(codePoint == 0x20BB7)
+    }
+  }
+
+  @Test
+  def testCodePointToSurrogatePair1(): Unit = {
+    val codePoint: Int = 0x20BB7
+    assert(Character.isValidCodePoint(codePoint))
+    if (Character.isValidCodePoint(codePoint)) {
+      val charArray: Array[Char] = Character.toChars(codePoint)
+      //IllegalArgumentException:
+      //codePointが有効なUnicodeコードポイントではない場合発生
+
+      assert(charArray.length == 2)
+      assert(charArray.head == 0xD842)
+      assert(charArray(1) == 0xDFB7)
+    }
+  }
+
+  @Test
+  def testCodePointToSurrogatePair2(): Unit = {
+    val codePoint: Int = 0x20BB7
+    val dst: Array[Char] = new Array(16)
+    val index: Int = 8
+    assert(Character.isValidCodePoint(codePoint))
+    if (Character.isValidCodePoint(codePoint)) {
+      //dst(index)またはdst(index)とdst(index + 1)に変換されたCharを代入し、代入したCharの個数を返す
+      val size = Character.toChars(codePoint, dst, index)
+      //IllegalArgumentException:
+      //codePointが有効なUnicodeコードポイントでない場合発生
+      //NullPointerException:
+      //dstがnullの場合発生
+      //IndexOutOfBoundsException:
+      //codePointがBMP領域なら0 ≦ dstIndex < dst.length、Supplementary領域なら0 ≦ dstIndex < dst.length - 1に反した場合発生
+
+      assert(size == 2)
+      assert(dst(8) == 0xD842)
+      assert(dst(9) == 0xDFB7)
+    }
+  }
+
+  @Test
+  def testCodePointToNumOfChars1(): Unit = {
+    val codePoint: Int = 0x20BB7
+    assert(Character.isValidCodePoint(codePoint))
+    if (Character.isValidCodePoint(codePoint)) {
+      val charArray: Array[Char] = Character.toChars(codePoint)
+      val length: Int = charArray.length
+      //IllegalArgumentException:
+      //codePointが有効なUnicodeコードポイントではない場合発生
+
+      assert(length == 2)
+    }
+  }
+
+  @Test
+  def testCodePointToNumOfChars2(): Unit = {
+    val codePoint: Int = 0x20BB7
+    val countOpt: Option[Int] =
+      if (Character.isValidCodePoint(codePoint)) {
+        Option(Character.charCount(codePoint))}
+      else {
+        None
+      }
+
+    assert(countOpt.nonEmpty)
+    if (countOpt.nonEmpty) {
+      assert(countOpt.get == 2)
+    }
+  }
+
+  @Test
+  def testCodePointToNumOfChars3(): Unit = {
+    val codePoint: Int = 0x20BB7
+    val countOpt: Option[Int] =
+      if (Character.isBmpCodePoint(codePoint)) {
+        Option(1)
+      } else if (Character.isSupplementaryCodePoint(codePoint)) {
+        Option(2)
+      } else {
+        None
+      }
+
+    assert(countOpt.nonEmpty)
+    if (countOpt.nonEmpty) {
+      assert(countOpt.get == 2)
+    }
+  }
+
+  @Test
+  def testCodePointToHighSurrogate(): Unit = {
+    val codePoint: Int = 0x20BB7
+    val high: Char = Character.highSurrogate(codePoint)
+
+    assert(Character.isSurrogate(high))
+    if (Character.isSurrogate(high)) {
+      assert(Character.isHighSurrogate(high))
+      if (Character.isHighSurrogate(high)) {
+        assert(high == 0xD842)
+      }
+    }
+  }
+
+  @Test
+  def testCodePointToLowSurrogate(): Unit = {
+    val codePoint: Int = 0x20BB7
+    val low: Char = Character.lowSurrogate(codePoint)
+
+    assert(Character.isSurrogate(low))
+    if (Character.isSurrogate(low)) {
+      assert(Character.isLowSurrogate(low))
+      if (Character.isLowSurrogate(low)) {
+        assert(low == 0xDFB7)
+      }
+    }
+  }
+
+  @Test
+  def testCharArrayElementToCodePointInForwardDirection1(): Unit = {
+    val charArray: Array[Char] = Array[Char](0xD842, 0xDFB7)
+    val index: Int = 0
+    val codePoint: Int = Character.codePointAt(charArray, index)
+    //NullPointerException:
+    //charArrayがnullの場合発生
+    //IndexOutOfBoundsException:
+    //0 ≦ index < charArray.lengthに反した場合発生
+
+    assert(codePoint == 0x20BB7)
+  }
+
+  @Test
+  def testCharArrayElementToCodePointInForwardDirection2(): Unit = {
+    val charArray: Array[Char] = Array[Char](0xD842, 0xDFB7, 'C')
+    var index: Int = 0
+    var limit: Int = 1
+    var codePoint: Int = Character.codePointAt(charArray, index, limit)
+    //NullPointerException:
+    //charArrayがnullである場合発生
+    //IndexOutOfBoundsException:
+    //0 ≦ index < limit < charArray.lengthに反した場合発生
+
+    assert(codePoint == 0xD842)
+
+    index = 0
+    limit = 2
+    codePoint = Character.codePointAt(charArray, index, limit)
+
+    assert(codePoint == 0x20BB7)
+
+    index = 1
+    limit = 3
+    codePoint = Character.codePointAt(charArray, index, limit)
+
+    assert(codePoint == 0xDFB7)
+  }
+
+  @Test
+  def testCharSequenceToCodePointInForwardDirection(): Unit = {
+    val seq: CharSequence = "CharSequenceは、String、StringBuilder、 StringBuffer、CharBufferなどが実装しているインターフェース"
+    val index: Int = 0
+    val codePoint: Int = Character.codePointAt(seq, index)
+    //NullPointerException:
+    //seqがnullである場合発生
+    //IndexOutOfBoundsException:
+    //0 ≦ index < seq.lengthに反した場合発生
+
+    assert(codePoint == 'C')
+  }
+
+  @Test
+  def testStringToCodePointInForwardDirection(): Unit = {
+    val str: String = "CharSequenceは、String、StringBuilder、 StringBuffer、CharBufferなどが実装しているインターフェース"
+    val index: Int = 0
+    val codePoint: Int = str.codePointAt(index)
+    //NullPointerException:
+    //strがnullである場合発生
+    //IndexOutOfBoundsException:
+    //0 ≦ index < str.lengthに反した場合発生
+
+    assert(codePoint == 'C')
+  }
+
+  @Test
+  def testCharArrayElementToCodePointInBackwardDirection1(): Unit = {
+    val charArray: Array[Char] = Array[Char](0xD842, 0xDFB7)
+    val index: Int = 1
+    val codePoint: Int = Character.codePointBefore(charArray, index)
+    //NullPointerException:
+    //charArrayがnullの場合発生
+    //IndexOutOfBoundsException:
+    //1 ≦ index ≦ charArray.lengthに反した場合発生
+
+    assert(codePoint == 0xD842)
+  }
+
+  @Test
+  def testCharArrayElementToCodePointInBackwardDirection2(): Unit = {
+    val charArray: Array[Char] = Array[Char]('C', 0xD842, 0xDFB7)
+    var index: Int = 3
+    var start: Int = 2
+    var codePoint: Int = Character.codePointBefore(charArray, index, start)
+    //NullPointerException:
+    //charArrayがnullである場合発生
+    //IndexOutOfBoundsException:
+    //0 ≦ start < index ≦ charArray.lengthに反した場合発生
+
+    assert(codePoint == 0xDFB7)
+
+    index = 3
+    start = 1
+    codePoint = Character.codePointBefore(charArray, index, start)
+
+    assert(codePoint == 0x20BB7)
+
+    index = 2
+    start = 0
+    codePoint = Character.codePointBefore(charArray, index, start)
+
+    assert(codePoint == 0xD842)
+  }
+
+  @Test
+  def testCharSequenceToCodePointInBackwardDirection(): Unit = {
+    val seq: CharSequence = "CharSequenceは、String、StringBuilder、 StringBuffer、CharBufferなどが実装しているインターフェース"
+    val index: Int = 1
+    val codePoint: Int = Character.codePointBefore(seq, index)
+    //NullPointerException:
+    //seqがnullである場合発生
+    //IndexOutOfBoundsException:
+    //1 ≦ index ≦ seq.lengthに反した場合発生
+
+    assert(codePoint == 'C')
+  }
+
+  @Test
+  def testStringToCodePointInBackwardDirection(): Unit = {
+    val str: String = "CharSequenceは、String、StringBuilder、 StringBuffer、CharBufferなどが実装しているインターフェース"
+    val index: Int = 1
+    val codePoint: Int = str.codePointBefore(index)
+    //NullPointerException:
+    //seqがnullである場合発生
+    //IndexOutOfBoundsException:
+    //1 ≦ index ≦ seq.lengthに反した場合発生
+
+    assert(codePoint == 'C')
+  }
+
+  @Test
+  def testStringToCharSequence(): Unit = {
+    //cast
+    val charSequence: CharSequence = "CharSequenceは、String、StringBuilder、 StringBuffer、CharBufferなどが実装しているインターフェース"
+
+  }
+
+  @Test
+  def testCharSequenceToString(): Unit = {
+    val charSequence: CharSequence = "CharSequenceは、String、StringBuilder、 StringBuffer、CharBufferなどが実装しているインターフェース"
+    val str: String = charSequence.toString
+
+    assert(charSequence == str)
+  }
+
+  @Test
+  def testCharSequenceToCharArray(): Unit = {
+    val charSequence: CharSequence = "𠮷野家"
+    val charArray: Array[Char] = charSequence.chars.toArray.map(_.toChar)
+
+    assert(charArray.length == 4)
+    assert(charArray.head == 0xD842)
+    assert(charArray(1) == 0xDFB7)
+    assert(charArray(2) == '野')
+    assert(charArray.last == '家')
+  }
+
+  @Test
+  def testStringToCharArray1(): Unit = {
+    val str: String = "𠮷野家"
+    val charArray: Array[Char] = str.toCharArray
+
+    assert(charArray.length == 4)
+    assert(charArray.head == 0xD842)
+    assert(charArray(1) == 0xDFB7)
+    assert(charArray(2) == '野')
+    assert(charArray.last == '家')
+  }
+
+  @Test
+  def testStringToCharArray2(): Unit = {
+    val str: String = "𠮷野家"
+    val charArray: Array[Char] = str.chars.toArray.map(_.toChar)
+
+    assert(charArray.length == 4)
+    assert(charArray.head == 0xD842)
+    assert(charArray(1) == 0xDFB7)
+    assert(charArray(2) == '野')
+    assert(charArray.last == '家')
+  }
+
+  @Test
+  def testCharArrayToString(): Unit = {
+    val charArray: Array[Char] = Array(0xD842.toChar, 0xDFB7.toChar, '野', '家')
+    val str = new String(charArray)
+
+    assert(str == "𠮷野家")
+  }
+
+  @Test
+  def testNumOfCharactersAndChars(): Unit = {
+    val strWithSurrogatePair: String = "𠮷野家"
+    val strWithoutSurrogatePair: String = "吉野家"
+
+    val numOfCharsOfStrWithSurrogatePair: Int = strWithSurrogatePair.length
+    val numOfCharsOfStrWithoutSurrogatePair: Int = strWithoutSurrogatePair.length
+
+    assert(numOfCharsOfStrWithSurrogatePair == 4)
+    assert(numOfCharsOfStrWithoutSurrogatePair == 3)
+    assert(numOfCharsOfStrWithoutSurrogatePair < numOfCharsOfStrWithSurrogatePair)
+
+    val numOfCharactersOfStrWithSurrogatePair: Int = strWithSurrogatePair.codePointCount(0, strWithSurrogatePair.length)
+    val numOfCharactersOfStrWithoutSurrogatePair: Int = strWithoutSurrogatePair.codePointCount(0, strWithoutSurrogatePair.length)
+
+    assert(numOfCharactersOfStrWithSurrogatePair == 3)
+    assert(numOfCharactersOfStrWithoutSurrogatePair == 3)
+    assert(numOfCharactersOfStrWithSurrogatePair == numOfCharactersOfStrWithoutSurrogatePair)
+  }
+
+  @Test
+  def testCodePointArrayToString(): Unit = {
+    val codePointArray: Array[Int] = Array(0x20BB7, '野', '家')
+    val str: String = new String(codePointArray, 0, codePointArray.length)
+
+    assert(str == "𠮷野家")
+  }
+
+  @Test
+  def testCharSequenceToCodePointArray(): Unit = {
+    val charSequence: CharSequence = "𠮷野家"
+    val codePointArray: Array[Int] = charSequence.codePoints().toArray
+
+    assert(codePointArray.length == 3)
+    assert(codePointArray.head == 0x20BB7)
+    assert(codePointArray(1) == '野')
+    assert(codePointArray.last == '家')
+  }
+
+  @Test
+  def testStringToCodePointArray(): Unit = {
+    val str: String = "𠮷野家"
+    val codePointArray: Array[Int] = str.codePoints().toArray
+
+    assert(codePointArray.length == 3)
+    assert(codePointArray.head == 0x20BB7)
+    assert(codePointArray(1) == '野')
+    assert(codePointArray.last == '家')
+  }
+}
